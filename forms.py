736967bfb8ef7090e00e69a89620b906e95b2b15
kosist <kosistivan@gmail.com>
2018-08-26 22:57:43 +0200

    Fixed issue with indentation in the classes definition.

--- forms.py
+++ forms.py
@@ -1,25 +1,17 @@
- from flask_wtf import FlaskForm
- from wtforms import StringField, PasswordField, SubmitField, BooleanField
- from wtforms.validators import DataRequired, Length, Email, EqualTo
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
- class RegistrationForm (FlaskForm):
- 	"""docstring for RegistrationForm """
-	username = StringField('Username',
-		validators=[DataRequired(), Length(min=2, max=20)])
-	email = StringField('Email',
-		validators=[DataRequired(), Email()])
-	password = PasswordField('Password',
-		validators=[DataRequired()])
-	confirm_password = PasswordField('Confirm Password',
-		validators=[DataRequired(), EqualTo('password')])
+class RegistrationForm (FlaskForm):
+	username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
 	submit = SubmitField('Sign Up')
 
- class LoginForm (FlaskForm):
- 	"""docstring for RegistrationForm """
-	email = StringField('Email',
-		validators=[DataRequired(), Email()])
-	password = PasswordField('Password',
-		validators=[DataRequired()])
+class LoginForm (FlaskForm):
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
 	remember = BooleanField('Remember Me')
 	submit = SubmitField('Login')
 
