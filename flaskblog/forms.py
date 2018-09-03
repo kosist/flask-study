1ef5625027d56ea62430dcd0206a8856c05af140
kosist <kosistivan@gmail.com>
2018-09-03 20:34:30 +0200

    Prepared form for request password reset

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -50,4 +50,21 @@ class PostForm(FlaskForm):
 	"""docstring for PostForm"""
 	title = StringField('Title', validators=[DataRequired()])
 	content = TextAreaField('Content', validators=[DataRequired()])
-	submit = SubmitField('Post') 		
+	submit = SubmitField('Post')
+
+class RequestResetForm(FlaskForm):
+	"""docstring for RequestResetForm"""
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	submit = SubmitField('Request Password Reset')
+
+	def validate_email(self, email):
+		user = User.query.filter_by(email = email.data).first()
+		if user is None:
+			raise ValidationError('There is no account with this email. You must register first')
+
+class ResetPasswordForm(FlaskForm):
+	"""docstring for ResetPasswordForm"""
+	password = PasswordField('Password', validators=[DataRequired()])
+	confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+	submit = SubmitField('Reset Password')
+		
