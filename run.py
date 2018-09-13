e472ccffe3d1d65a683b345ac558acdc7d4a6ad1
kosist <kosistivan@gmail.com>
2018-09-13 21:54:26 +0200

    Implemented function for configuration of the whole application

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
-from flaskblog import app
+from flaskblog import create_app
+
+#Now it uses default cfg class. But, we could
+#pass in here some another configuration class
+app = create_app()
 
 if __name__ == '__main__':
 	app.run(debug=True)
